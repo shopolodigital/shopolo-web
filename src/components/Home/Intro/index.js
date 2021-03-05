f9116db028536796103dcd224761e36e5c74ac89
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2021-03-05 11:12:46 +0530

    intro page desktop view

--- src/components/Home/Intro/index.js
+++ src/components/Home/Intro/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./Intro.scss"
+import IntroImage from "../../../static/IntroImage.svg";
 
 const Intro = () => {
     return (
         <div className="container">
-            
+            <div className="DetalsContainer">
+                <div className="Text">
+                    <p className="Heading">Distribute Your Music <br/> Everywhere For Free</p>
+                    <p className="Body">
+                        <b>Hey there! This is Shopolo Music.</b><br/>
+                        One of the best and recommended music distribution platforms <br/> for individual creators and labels.
+                    </p>
+                    <button className="SignUpButton">SIGN UP</button>
+                </div>
+            </div>
+            <div className="ImageContainer">
+                <img src={IntroImage} alt="Welcome"/>
+            </div>
+            <div className="circularGrad1"/>
+            <div className="circularGrad2"/>
         </div>
     )
 }
